95b370c4fdf59890bc2551660e3877e115c548a3
Dashline Jove D Souza <dashline.souza@quest.com>
2025-06-18 09:48:34 +0530

    added requiredArgConstructor Annotation and removed manually coded constructor

--- user-service/src/main/java/com/ticketnest/user_service/Controller/UserController.java
+++ user-service/src/main/java/com/ticketnest/user_service/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.ticketnest.user_service.Controller;
 import com.ticketnest.user_service.Service.UserService;
 import com.ticketnest.user_service.dto.RegisterRequest;
 import com.ticketnest.user_service.dto.RegisterResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request)
     {
