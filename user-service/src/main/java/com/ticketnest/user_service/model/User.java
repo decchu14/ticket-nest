85a65cb743837ccaac59b767d1d0fcf165dc0cb9
Dashline Jove D Souza <dashline.souza@quest.com>
2025-06-18 08:01:48 +0530

    created model package and simple user model/entity

---
+++ user-service/src/main/java/com/ticketnest/user_service/model/User.java
@@ -0,0 +1,28 @@
+package com.ticketnest.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+
+    private String role;
+}
