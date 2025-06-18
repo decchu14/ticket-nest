9bf4972ad1921b2ceacb64f6cdbbbd4e9b32a6c3
Dashline Jove D Souza <dashline.souza@quest.com>
2025-06-18 09:50:05 +0530

    added postmapping

--- user-service/src/main/java/com/ticketnest/user_service/Controller/UserController.java
+++ user-service/src/main/java/com/ticketnest/user_service/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.ticketnest.user_service.dto.RegisterRequest;
 import com.ticketnest.user_service.dto.RegisterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request)
     {
         RegisterResponse result = userService.register(request);
